5584342125214fbf1581947259dc33c5b749897f
snopan <snopan@protonmail.com>
2023-08-30 09:45:28 +1000

    Added util fn to get frame, health, name, box

--- src/card.rs
+++ src/card.rs
@@ -6,7 +6,7 @@ use rusttype::{Font, Scale};
 use text_render::{render::{render_draw_text_inputs}};
 
 
-pub mod layout;
+mod layout;
 mod operations;
 
 pub struct FontConfig<'a> {
@@ -17,26 +17,29 @@ pub struct FontConfig<'a> {
 
 pub fn make_card(
     name: &str,
-    avatar: &str,
-    frame: &str,
-    health: &str,
     num_health: u32,
     skills: String,
+    avatar_path: &str,
+    frame_path: &str,
+    health_path: &str,
+    name_color: Rgba<u8>,
+    box_color: Rgba<u8>,
+    card_width: u32,
     fonts: FontConfig,
-    layout: &layout::Layout
 ) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
     
+    let layout = layout::new_layout(card_width);
     let mut card = RgbaImage::new(layout.card_width(), layout.card_height());
     
     operations::draw_avatar(
         &mut card,
-        avatar,
+        avatar_path,
         (layout.content_top_left_x() as i64, layout.content_top_left_y() as i64),
         layout.content_width() as f32,
         layout.content_height() as f32
     );
 
-    operations::draw_frame(&mut card, frame);
+    operations::draw_frame(&mut card, frame_path);
 
     let (draw_text_inputs, skills_text_height) = operations::generate_draw_text_details(
         skills,
@@ -62,7 +65,7 @@ pub fn make_card(
         layout.skills_box_outline(),
         layout.skills_box_gap(),
         layout.skills_box_corner(),
-        Rgba([255u8, 255u8, 255u8, 150u8])
+        box_color
     );
 
     render_draw_text_inputs(
@@ -84,14 +87,14 @@ pub fn make_card(
         }, 
         fonts.font_title,
         name,
-        Rgba([136u8, 8u8, 8u8, 255u8]),
+        name_color,
         layout.name_inner_outline() as i32,
         layout.name_outer_outline() as i32,
     );
 
     operations::draw_health(
         &mut card,
-        health,
+        health_path,
         num_health,
         layout.health_top_right_x() as i64,
         layout.health_top_right_y(skills_text_height) as i64,
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use card::FontConfig;
-use image::{Pixel};
+use image::{Pixel, Rgba};
 use rusttype::{Font};
 
 mod config;
@@ -20,17 +20,33 @@ fn main() {
         font_title: &font_title
     };
 
-    let layout = card::layout::new_layout(1024);
+    let character = &config.characters[0];
 
     let card = card::make_card(
-        config.characters[0].name.as_str(),
-        config.characters[0].picture.as_str(),
-        config.assets.frames.shu.as_str(),
-        config.assets.health.shu.as_str(),
-        config.characters[0].health,
-        make_skills_text(&config.characters[0].skills),
-        fonts,
-        &layout,
+        character.name.as_str(),
+        character.health,
+        make_skills_text(&character.skills),
+        character.picture.as_str(),
+        get_frame_path(
+            character.kingdom.as_str(),
+            character.monarch,
+            &config.assets.frames
+        ),
+        get_health_path(
+            character.kingdom.as_str(),
+            character.monarch,
+            &config.assets.health,
+        ),
+        get_name_color(
+            character.kingdom.as_str(),
+            character.monarch,
+        ),
+        get_box_color(
+            character.kingdom.as_str(),
+            character.monarch,
+        ),
+        1024,
+        fonts
     );
 
     card.save(String::from("./output/image.png")).unwrap();
@@ -41,4 +57,60 @@ fn make_skills_text(skills: &Vec<config::Skill>) -> String {
         let name = s.name.split(' ').map(|x| format!("@bold;{}", x)).collect::<Vec<String>>().join(" ");
         format!("{}\n{}", name, s.description)
     }).collect::<Vec<String>>().join("\n\n")
+}
+
+fn get_frame_path<'a>(kingdom: &str, monarch: bool, frames: &'a config::Frames) -> &'a str {
+    match (kingdom, monarch) {
+        ("kingdomless", false) => frames.kingdomless.as_str(),
+        ("shu", false) => frames.shu.as_str(),
+        ("wei", false) => frames.wei.as_str(),
+        ("wu", false) => frames.wu.as_str(),
+        ("kingdomless", true) => frames.kingdomless_zhu.as_str(),
+        ("shu", true) => frames.shu_zhu.as_str(),
+        ("wei", true) => frames.wei_zhu.as_str(),
+        ("wu", true) => frames.wu_zhu.as_str(),
+        _ => panic!("Not a valid kingdom!")
+    }
+}
+
+fn get_health_path<'a>(kingdom: &str, monarch: bool, health: &'a config::Health) -> &'a str {
+    if monarch {
+        return health.zhu.as_str();
+    }
+    
+    match kingdom {
+        "kingdomless" => health.kingdomless.as_str(),
+        "shu" => health.shu.as_str(),
+        "wei" => health.wei.as_str(),
+        "wu" => health.wu.as_str(),
+        _ => panic!("Not a valid kingdom!")
+    }
+}
+
+fn get_name_color(kingdom: &str, monarch: bool) -> Rgba<u8> {
+    if monarch {
+        return Rgba([]);
+    }
+    
+    match kingdom {
+        "kingdomless" => Rgba([]),
+        "shu" => Rgba([]),
+        "wei" => Rgba([]),
+        "wu" => Rgba([]),
+        _ => panic!("Not a valid kingdom!")
+    }
+}
+
+fn get_box_color(kingdom: &str, monarch: bool) -> Rgba<u8> {
+    if monarch {
+        return Rgba([]);
+    }
+    
+    match kingdom {
+        "kingdomless" => Rgba([]),
+        "shu" => Rgba([]),
+        "wei" => Rgba([]),
+        "wu" => Rgba([]),
+        _ => panic!("Not a valid kingdom!")
+    }
 }
