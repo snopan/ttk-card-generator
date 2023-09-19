f54fc7fc2b9485888060d27722eae33607cd785e
snopan <snopan@protonmail.com>
2023-09-19 23:08:51 +1000

    Added more asset path and setup multi thread generation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 
+use std::thread;
+
 use card::FontConfig;
 
 mod config;
@@ -16,30 +18,37 @@ fn main() {
     let font_bold = font::load_font(assets.fonts.bold).unwrap();
     let font_title = font::load_font(assets.fonts.title).unwrap();
 
-    let fonts: FontConfig = FontConfig{
-        font_regular: &font_regular,
-        font_bold: &font_bold,
-        font_title: &font_title
-    };
+    let handle = thread::spawn(move || {
+        for c in characters.list {
+            let name = c.name.as_str();
+            let avatar_path = character::get_avatar_path(name, &assets.characters);
+            let kingdom = c.kingdom.as_str();
+            let monarch = c.monarch;
 
-    let c = &characters.list[0];
-    let name = c.name.as_str();
-    let kingdom = c.kingdom.as_str();
-    let monarch = c.monarch;
+            println!("Creating card {}", name);
+            println!("With avatar {}", avatar_path);
 
-    let card = card::make_card(
-        name,
-        c.health,
-        character::make_skills_text(&c.skills),
-        character::get_avatar_path(name, &assets.characters),
-        character::get_frame_path(kingdom, monarch, &assets.frames),
-        character::get_health_path(kingdom, monarch, &assets.health),
-        character::get_gender_path(kingdom, monarch, c.male, &assets.genders),
-        character::get_name_color(kingdom, &styles.name_outline_colors),
-        character::get_box_color(kingdom, monarch, &styles.skill_box_colors),
-        1024,
-        fonts
-    );
+            let card = card::make_card(
+                name,
+                c.health,
+                character::make_skills_text(&c.skills),
+                avatar_path,
+                character::get_frame_path(kingdom, monarch, &assets.frames),
+                character::get_health_path(kingdom, monarch, &assets.health),
+                character::get_gender_path(kingdom, monarch, c.male, &assets.genders),
+                character::get_name_color(kingdom, &styles.name_outline_colors),
+                character::get_box_color(kingdom, monarch, &styles.skill_box_colors),
+                1024,
+                FontConfig{
+                    font_regular: &font_regular,
+                    font_bold: &font_bold,
+                    font_title: &font_title
+                }
+            );
 
-    card.save(String::from("./output/image.png")).unwrap();
+            card.save(String::from(format!("./output/{}.png", name))).unwrap();
+        }
+    });
+
+    handle.join().unwrap();
 }
